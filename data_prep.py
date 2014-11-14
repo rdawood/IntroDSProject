b5c39cdfffc3098dd93e57ab1897f367e9629d1b
rdawood <raza.dawood@nyu.edu>
2014-11-14 17:40:27 -0500

    First Commit

---
+++ data_prep.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+from pandas import DataFrame
+import numpy as np 
+
+import os 
+
+
+DFs = {}
+
+
+# for root, dirs, files in os.walk("data/"):
+# 	for csv in files:
+# 		print csv
+# 		df = pd.read_csv("data/" + str(csv))
+# 		DFs[str(csv)] = df
+
+
+# for df in DFs:
+# 	DFs[df].to_pickle("data/" + str(df)[-4])
+
+
