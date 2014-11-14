d9549a94e8ff3883c4aa888e58572bd10a960f92
rdawood <raza.dawood@nyu.edu>
2014-11-14 17:54:31 -0500

    Data Prep file

--- data_prep.py
+++ data_prep.py
@@ -1,21 +1,28 @@
 import pandas as pd 
 from pandas import DataFrame
 import numpy as np 
-
 import os 
 
 
 DFs = {}
 
-
-# for root, dirs, files in os.walk("data/"):
-# 	for csv in files:
-# 		print csv
-# 		df = pd.read_csv("data/" + str(csv))
-# 		DFs[str(csv)] = df
-
-
-# for df in DFs:
-# 	DFs[df].to_pickle("data/" + str(df)[-4])
+# set 0 to 1 to write csv's to pickles
+if 1 == 0:
+	for root, dirs, files in os.walk("data/"):
+		for csv in files:
+			print csv
+			df = pd.read_csv("data/" + str(csv))
+			DFs[str(csv)] = df
 
 
+	for df in DFs:
+		DFs[df].to_pickle("data/" + str(df)[-4])
+
+# set 0 to 1 to read pickles as dataframes
+if 1 == 0:
+	for root, dirs, files in os.walk("data/"):
+		for serial in files:
+			df = pd.read_pickle("data/" + str(serial))
+			DFs[str(serial)] = df
+
+print DFs.keys()
