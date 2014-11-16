a77def99bc55d53e0467953a814d8e5f6e59b105
rdawood <raza.dawood@nyu.edu>
2014-11-15 20:00:38 -0500

    Wrote functions for pickling and reading pickles

--- data_prep.py
+++ data_prep.py
@@ -4,10 +4,10 @@ import numpy as np
 import os 
 
 
-DFs = {}
 
-# set 0 to 1 to write csv's to pickles
-if 1 == 0:
+def readCSV():
+	DFs = {}
+
 	for root, dirs, files in os.walk("data/"):
 		for csv in files:
 			print csv
@@ -18,11 +18,17 @@ if 1 == 0:
 	for df in DFs:
 		DFs[df].to_pickle("data/pickled/" + str(df)[-4])
 
-# set 0 to 1 to read pickles as dataframes
-if 1 == 0:
+def readPickles():
+	DFs = {}
+
 	for root, dirs, files in os.walk("data/pickled/"):
 		for serial in files:
 			df = pd.read_pickle("data/pickled/" + str(serial))
 			DFs[str(serial)] = df
 
-print DFs.keys()
+	return DFs()
+
+
+#readCSV()
+
+#readPickles()
