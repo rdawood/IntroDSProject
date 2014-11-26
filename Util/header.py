62081f7bffac27e3ffe0f98685fb7200f2bdf706
im965 <israelmalkin@nyu.edu>
2014-11-25 19:53:39 -0500

    Added header in utils & full.csv in MasterData

---
+++ Util/header.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 25 19:48:33 2014
+
+@author: Israel
+
+This function will load all the necessary python packages
+"""
+
+def tools():
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+
